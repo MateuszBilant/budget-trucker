20cf5bdfeb583ad5934de4a00cc972ad217a4c1b
Mateusz Bilant <mateuszbilant@gmail.com>
2026-03-13 17:52:34 +0100

    fix: initialized instances of repos in ioc.ts and fixed TransactionController.ts

--- src/ioc.ts
+++ src/ioc.ts
@@ -1,5 +1,21 @@
 import { PrismaClient } from "@prisma/client";
 import { TransactionController } from "./controllers/transaction.controller";
+import { TransactionRepository } from "./repositories/transaction.repository";
+import { TransactionService } from "./services/transaction.service";
+import { clock } from "./utils/clock";
+import { UserRepository } from "./repositories/user.repository";
+import { CategoryRepository } from "./repositories/category.repository";
 
 export const prismaClient = new PrismaClient();
-export const transactionController = new TransactionController();
+export const transactionRepo = new TransactionRepository(prismaClient, clock);
+export const userRepo = new UserRepository(prismaClient);
+export const categoryRepo = new CategoryRepository(prismaClient);
+export const transactionService = new TransactionService({
+  transactionRepo,
+  userRepo,
+  categoryRepo,
+  clock: clock,
+});
+export const transactionController = new TransactionController(
+  transactionService,
+);
